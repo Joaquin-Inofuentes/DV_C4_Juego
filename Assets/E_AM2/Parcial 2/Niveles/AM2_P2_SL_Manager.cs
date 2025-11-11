cf549d29ce4188b21fb830a486abe53300b2fcc1
JoaquinDavinci <joaquin.inofuentes@davinci.edu.ar>
2025-11-11 20:44:49 -0300

    Tienda y selector de niveles

---
+++ Assets/E_AM2/Parcial 2/Niveles/AM2_P2_SL_Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AM2_P2_SL_Manager : MonoBehaviour
+{
+    public static event Action<string> Selecciono1Nivel;
+
+    public void Seleccionar1Nivel(string nombreDeLaEscena)
+    {
+        Debug.Log($"🔹 Seleccionar1Nivel llamado con: {nombreDeLaEscena}");
+        Selecciono1Nivel?.Invoke(nombreDeLaEscena);
+    }
+}
---
+++ Assets/E_AM2/Parcial 2/Tienda/AM_P2_Tienda_Visual.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AM_P2_Tienda_Visual : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
