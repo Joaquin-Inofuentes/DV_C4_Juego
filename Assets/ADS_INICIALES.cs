57f770f78a9357446a5d1f9b574055edab698a3c
Leon-330 <Carlos.funes@davinci.edu.ar>
2026-03-04 21:58:28 -0300

    Bug de notificaciones

---
+++ Assets/ADS_INICIALES.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ADS_INICIALES : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AM2_P2_AdsManager.Instance.ShowBanner();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
